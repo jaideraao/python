84b6a8cde2f8c2e34d44d64d04112753b4516f6e
Jaider Angarita <jaideraao@icloud.com>
2023-07-07 17:09:04 -0500

    modified: app/app.py

--- app/app.py
+++ app/app.py
@@ -1,2 +1,8 @@
-msg = "Hola!."
-print(msg)
+import json
+import requests
+
+response = requests.get("https://62433a7fd126926d0c5d296b.mockapi.io/api/v1/usuarios")
+
+data = response.json()
+
+print(data)
